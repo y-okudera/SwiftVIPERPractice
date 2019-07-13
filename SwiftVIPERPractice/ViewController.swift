b4001e67f397885ddcaf65e872150d2a2c95566d
y-okudera <appledev.yuoku@gmail.com>
2019-07-13 17:30:18 +0900

    Initial Commit

---
+++ SwiftVIPERPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftVIPERPractice
+//
+//  Created by Yuki Okudera on 2019/07/13.
+//  Copyright © 2019 Yuki Okudera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
